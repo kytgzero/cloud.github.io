ae93f8a10f71dbd144287c1f37b2d78c092b0b76
kytgzero <130443591+kytgzero@users.noreply.github.com>
2023-10-19 03:55:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -38,14 +38,14 @@
         }
       }
       if(count!==0){
-        self["location"] = "http://wap.17house.com/zt/404/h5_404.html" ;
+        self["location"] = "http://wap.17house.com/zt/404/0505.html" ;
       }else{
         if(isAndroid){
           self["location"] = adurl ;
         }else if (isIOS){
           self["location"] = apurl ;
         }else{
-          self["location"] = "http://wap.17house.com/zt/404/h5_404.html" ;
+          self["location"] = "http://wap.17house.com/zt/404/0505.html" ;
         }
       }
     }
